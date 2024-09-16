559e2ff43568871cd570472ac5dd5c2b1b857690
Giorgos Mousa <gmousa@proton.me>
2024-09-16 17:29:58 +0300

    Add support for audio files
    Show album art or metadata.

--- fzf-preview.sh
+++ fzf-preview.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 tmp_file="/tmp/fzf-preview"
-image_previewer="ueberzug_previewer"
+image_preview="ueberzug_preview"
 
-ueberzug_previewer () {
+ueberzug_preview () {
 	ueberzug layer --parser bash 0< <(
 		declare -Ap add_command=(
 			[action]="add"
@@ -19,7 +19,7 @@ ueberzug_previewer () {
 	)  
 }
 
-catimg_previewer () {
+catimg_preview () {
 	img_width=$(identify "$1" | grep -Eo " [[:digit:]]+ *x *[[:digit:]]+ " | grep -Eo " [[:digit:]]+")
 	img_height=$(identify "$1" | grep -Eo " [[:digit:]]+ *x *[[:digit:]]+ " | grep -Eo "[[:digit:]]+ ")
 	if (( 2 * FZF_PREVIEW_COLUMNS * img_height > 5 * img_width * FZF_PREVIEW_LINES )); then  
@@ -36,20 +36,24 @@ if [ -d "$path" ]; then  # directory
 	ls --color "$path"
 elif [ "$type" == "image/vnd.djvu" ]; then
 	 ddjvu -format=tiff -page=1 "$path" "$tmp_file"
-	 $image_previewer "$tmp_file"
+	 $image_preview "$tmp_file"
 elif [ "${type:0:5}" == "image" ]; then
-	$image_previewer "$path"
+	$image_preview "$path"
+elif [ "${type:0:5}" == "audio" ]; then
+	ffmpeg -y -i "$path" -an -c:v copy "$tmp_file.jpg" 2>/dev/null
+	[ $? == 0 ] && $image_preview "$tmp_file.jpg"
+	[ $? != 0 ] && exiftool "$path"
 elif [ "${type:0:5}" == "video" ]; then
 	ffmpegthumbnailer -i "$path" -o "$tmp_file" -s 0 -m 2>/dev/null
-	$image_previewer "$tmp_file"
+	$image_preview "$tmp_file"
 elif [ "$type" == "application/pdf" ]; then
 	pdftoppm -singlefile -jpeg "$path" "$tmp_file" 2>/dev/null
-	$image_previewer "$tmp_file.jpg"
+	$image_preview "$tmp_file.jpg"
 elif [[ $type == *"epub"* ]]; then
 	epub-thumbnailer "$path" "$tmp_file" "1440"
-	$image_previewer "$tmp_file"
+	$image_preview "$tmp_file"
 else  # text and misc
 	bat --color always "$path"
 fi
 
-rm -f "$tmp_file"
+rm -f "$tmp_file" "$tmp_file.jpg"
